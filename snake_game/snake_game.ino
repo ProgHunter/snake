9affbcaa7a1400f01279dbb3f47ff579d3616928
Pierre-Yves Vincent-Tremblay <py.vtremblay@gmail.com>
2019-04-23 00:44:15 -0400

    Continue the implementation of the game. The snake's body will be a FIFO.. for now.

--- snake_game/snake_game.ino
+++ snake_game/snake_game.ino
@@ -4,45 +4,175 @@
 // Date: 20 avril 2019
 // --------------------------------------------------------------- //
 
-// Constants related to the pin mapping of the 74595 chip -> dot-matri
-const char LATCH_PIN = 8;   //Pin connected to ST_CP(RCLK) of 74HC595
-const char CLOCK_PIN = 12;  //Pin connected to SH_CP(SRCLK) of 74HC595
-const char DATA_PIN = 11;   //Pin connected to DS(SER) of 74HC595
+// Constant related to the square dot-matrix size
+#define MATRIX_MIN 0
+#define MATRIX_MAX 7
+#define MATRIX_SIZE 8
+
+// Constants related to the pin mapping of the 74595 chip -> dot-matrix
+#define LATCH_PIN 8   //Pin connected to ST_CP(RCLK) of 74HC595
+#define CLOCK_PIN 12  //Pin connected to SH_CP(SRCLK) of 74HC595
+#define DATA_PIN 11   //Pin connected to DS(SER) of 74HC595
 
 // Constants related to the pin mapping of the joystick
-const char JOY_X_PIN = A0;  // X axis of the joystick
-const char JOY_Y_PIN = A1;  // Y axis of the joystick
-const char JOY_B_PIN = 7;   // Button of the joystick
+#define JOY_X_PIN A0  // X axis of the joystick
+#define JOY_Y_PIN A1  // Y axis of the joystick
+#define JOY_B_PIN 7   // Button of the joystick
 
 // Difficulty of the game
 uint8_t difficulty;
+// Representing the position of the dot to eat
+Coord random_dot_position;
+// Dirrection of a moovement
+enum mvt_dir { left, down, right, up, unknown };
+
 // Grid representing the 8x8 dot-matrix
-uint8_t grid[8] = {0};
-// Number from 0 to 63 representing the position of the dot to eat
-uint8_t random_dot_position;
+//       x =
+// y = 0\0 1 2 3 ...
+//     1
+//     2
+//     ...        7\7
+//
+class Grid {
+  uint8_t grid[];
+
+  public:
+  Grid() {
+    grid[MATRIX_SIZE] = {0};
+  }
+
+  ~Grid() {
+    delete[] grid;
+  }
+  
+  // Activate a dot on the grid at a position
+  void add_dot(Coord *pos) {
+    grid[pos->y] |= (1 << pos->x);
+  }
+
+  // Deactivate a dot on the grid at a position
+  void delete_dot(Coord *pos) {
+    grid[pos->y] &= ~(1 << pos->x);
+  }
+};
+
+// X and Y coordinates in the 8x8 matrix
+class Coord {
+  uint8_t x;
+  uint8_t y;
+
+  public:
+  Coord(uint8_t init_x, uint8_t init_y) {
+    x = init_x;
+    y = init_y;
+  }
+
+  ~Coord() {
+    delete x;
+    delete y;
+  }
+
+  bool setCoord_XY(uint8_t new_x, uint8_t new_y) {
+      if(   new_x < MATRIX_MIN
+         || new_x > MATRIX_MAX
+         || new_y < MATRIX_MIN
+         || new_y > MATRIX_MAX)
+        return true;
+      x = new_x;
+      y = new_y;
+      return false;
+  }
+  
+  bool moove(enum mvt_dir moovement_direction) {
+    bool error = false;
+    switch(moovement_direction) {
+      case left:
+        if(x <= MATRIX_MIN)
+          error = true;
+        else
+          x--;
+        break;
+      case down:
+        if(y >= MATRIX_MAX)
+          error = true;
+        else
+          y++;
+        break;
+      case right:
+        if(x >= MATRIX_MAX)
+          error = true;
+        else
+          x++;
+        break;
+      case up:
+        if(y <= MATRIX_MIN)
+          error = true;
+        else
+          y--;
+        break;
+      default:
+        error = true;
+    }
+    return error;
+  }
+};
+
+// A part of the snake
+Class Body {
+  Coord *part;
+  Coord *next;
+
+  public:
+  Body(Coord *init_part) {
+    part = init_head;
+    next = NULL;
+  }
+
+  ~Body() {
+    if(part != NULL)
+      part.~Coord();
+    if(next != NULL)
+      next.~Coord();
+  }
+
+  void addNext(Coord *new_next) {
+    next = new_next;
+  }
+};
+
 // A gentle snake to play with
+// Implemented as a FIFO (for the moment)
 class Snake {
-  // Number representing the direction of the next movement
-  // 0 == Left, 1 == Down, 2 == Right, 3 == Up
-  uint8_t movement_direction;
   // Tab that represent the dots that are occupied by the snake on the grid
-  uint8_t position_in_grid[8] = {0};
-  // Number from 0 to 63 representing the snake's head position
-  uint8_t head;
-  // Number from 0 to 63 representing the snake's tail position
-  uint8_t tail;
+  // Keeped in sync with the body position when using the snake's functions
+  Grid positions;
+  enum mouvement_direction next_head_dir;
+  uint_8 body_size;
+  // The snake's head position and next body part
+  Body *head;
 
   // Construtor of the snake
   public:
-  Snake(uint8_t init_direction, uint8_t init_position) {
-    movement_direction = init_direction;
-    positionToGrid(&position_in_grid, init_position);
-    head = init_position;
-    tail = init_position;
+  Snake(enum mouvement_direction init_direction, Body *init_head) {
+    next_head_dir = init_direction;
+    head = init_head;
+    body_size = 1;
+    &positions = new Grid();
+    positions.add_dot(head->part);
   }
 
-  void move_update(){
-    
+  ~Snake() {
+    delete next_head_dir;
+    delete[] position_in_grid;
+    delete body_size;
+    if(*head != NULL)
+      head->~Coord();  // legit ?
+  }
+  
+  bool move_update(){
+    // - TODO: FIFO
+    // - Note, cannot go to head->next->part coord if != NULL
+    // - Change for new head (set next), then erase the "next" of body at body_size-1
   }
 };
 
@@ -64,7 +194,3 @@ void loop() {
 }
 
 //---FUNCTIONS---//
-// Transform a position number into the correspnding position in the 8x8 grid
-positionToGrid(uint8_t *grid[], uint8_t position_num) {
-  
-}
